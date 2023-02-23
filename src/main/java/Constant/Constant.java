4b7ca6c3fa5f91168ccf04ca30020f904d2be283
Neplim@gmail.com <V@M_P@ssw0rD>
2023-02-23 22:03:29 +0400

    update path for config.json

--- src/main/java/Constant/Constant.java
+++ src/main/java/Constant/Constant.java
@@ -2,5 +2,5 @@ package Constant;
 
 public class Constant {
 
-    public static final String CONFIG_PATH = "src/main/resources/config.json";
+    public static final String CONFIG_PATH = "./config.json";
 }
