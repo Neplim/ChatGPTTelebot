917583c44d81350d4d8aa8797b9aa6693b00ee23
Neplim@gmail.com <V@M_P@ssw0rD>
2023-02-24 11:18:26 +0400

    fix path

--- src/main/java/Common/Constant.java
+++ src/main/java/Common/Constant.java
@@ -3,7 +3,7 @@ package Common;
 public class Constant {
 
     public static final String ACCESS_DENIED = " You are denied access or access from this chat is denied.";
-    public static final String CONFIG_PATH = "src/main/resources/config.json";
+    public static final String CONFIG_PATH = "./config.json";
     public static final String ON_LISTEN = "Bot listen";
 
 }
